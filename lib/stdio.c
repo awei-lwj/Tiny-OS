4616878e665b3e993aef8467d056736dae25b456
awei-lwj <1732450197@qq.com>
2021-07-17 11:35:25 +0800

    实现printf的输出函数

--- lib/stdio.c
+++ lib/stdio.c
@@ -5,6 +5,7 @@
 #include "../lib/user/syscall.h"
 #include "print.h"
 
+
 #define va_start(ap, v) ap = (va_list)&v     // 把ap指向第一个固定参数v
 #define va_arg(ap, t) *((t *)(ap += 4))      // ap指向下一个参数并返回其值
 #define va_end(ap) ap = NULL                 // 清除ap
